56fa553e924b470b5221da71feaeecfaf62f5505
Cat <32003307+defnotcat@users.noreply.github.com>
2021-11-14 17:40:49 +0100

    Add HexExtensions

---
+++ Shadow.Client/Extensions/HexExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Shadow.Client.Extensions
+{
+    public static class HexExtensions
+    {
+        public static string HexEncode(this byte[] byteArr, string sep = "")
+            => BitConverter.ToString(byteArr).Replace("-", sep);
+
+        public static byte[] HexDecode(this string hex)
+            => Enumerable.Range(0, hex.Length)
+                .Where(x => x % 2 == 0)
+                .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                .ToArray();
+    }
+}
